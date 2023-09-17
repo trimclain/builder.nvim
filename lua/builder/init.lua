515c1e8dd3b030aac0188a354a7a938d3cbef1b6
Arthur McLain <mclain.it@gmail.com>
2023-09-18 00:41:25 +0200

    feat: add major updates for first release

--- lua/builder/init.lua
+++ lua/builder/init.lua
@@ -1,121 +1,75 @@
 local M = {}
 
 local config = {
-    terminal = {
-        position = "bot",
-        line_no = false,
-        size = 10,
-    },
+    position = "bot", -- "bot, top or vert"
+    size = 10, -- size in lines for position = "bot" and in characters for position = "vert"
+    line_no = false, -- show line numbers
+    autosave = true, -- automatically save before building
+    enable_internals = true, -- use neovim's internal `:luafile %` and `:source %`
+    commands = {},
 }
 
 function M.setup(opts)
-    opts = vim.tbl_deep_extend("force", config, opts or {})
+    config = vim.tbl_deep_extend("force", config, opts or {})
 end
 
--- local function dimensions(opts)
---     local cl = vim.o.columns
---     local ln = vim.o.lines
---     local width = math.ceil(cl * opts.ui.float.width)
---     local height = math.ceil(ln * opts.ui.float.height - 4)
---     local col = math.ceil((cl - width) * opts.ui.float.x)
---     local row = math.ceil((ln - height) * opts.ui.float.y - 1)
---     return {
---         width = width,
---         height = height,
---         col = col,
---         row = row,
---     }
--- end
-
--- local function resize()
---     local dim = dimensions(config)
---     vim.api.nvim_win_set_config(M.win, {
---         style = "minimal",
---         relative = "editor",
---         border = config.ui.float.border,
---         height = dim.height,
---         width = dim.width,
---         col = dim.col,
---         row = dim.row,
---     })
--- end
-
--- local function substitute(cmd)
---     cmd = cmd:gsub("%%", vim.fn.expand("%"))
---     cmd = cmd:gsub("$fileBase", vim.fn.expand("%:r"))
---     cmd = cmd:gsub("$filePath", vim.fn.expand("%:p"))
---     cmd = cmd:gsub("$file", vim.fn.expand("%"))
---     cmd = cmd:gsub("$dir", vim.fn.expand("%:p:h"))
---     cmd = cmd:gsub(
---         "$moduleName",
---         vim.fn.substitute(
---             vim.fn.substitute(vim.fn.fnamemodify(vim.fn.expand("%:r"), ":~:."), "/", ".", "g"),
---             "\\",
---             ".",
---             "g"
---         )
---     )
---     cmd = cmd:gsub("#", vim.fn.expand("#"))
---     cmd = cmd:gsub("$altFile", vim.fn.expand("#"))
---     return cmd
--- end
-
-local function term(cmd)
-    vim.cmd(config.terminal.position .. " " .. config.terminal.size .. "new | term " .. cmd)
-
-    local buf = vim.api.nvim_get_current_buf()
-
-    vim.bo[buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = buf, silent = true })
-    -- vim.keymap.set("n", "<ESC>", "<cmd>close<cr>", { buffer = buf, silent = true })
-    -- vim.api.nvim_buf_set_keymap(buf, "n", "<ESC>", "<cmd>:bdelete!<CR>", { silent = true })
-
-    -- if not config.ui.terminal.line_no then
-    --     vim.cmd("setlocal nonumber | setlocal norelativenumber")
-    -- end
+--- Use vim.notify to send INFO notifications
+---@param msg string
+local info = function(msg)
+    vim.notify(msg, vim.log.levels.INFO, { title = "Builder" })
 end
 
-local function run(cmd)
-    cmd = cmd or config.cmds.external[vim.bo.filetype]
+local function substitute(cmd)
+    -- :t is needed because when you open a file using a file tree, % becomes full path to the file
+    cmd = cmd:gsub("%%", vim.fn.expand("%:t"))
+    cmd = cmd:gsub("$file", vim.fn.expand("%:t"))
+    cmd = cmd:gsub("$ext", vim.fn.expand("%:e"))
+    cmd = cmd:gsub("$basename", vim.fn.expand("%:t:r"))
+    cmd = cmd:gsub("$path", vim.fn.expand("%:p"))
+    cmd = cmd:gsub("$dir", vim.fn.expand("%:p:h"))
+    return cmd
+end
 
-    if not cmd then
-        vim.cmd("echohl ErrorMsg | echo 'Error: Invalid command' | echohl None")
+function M.build(opts)
+    opts = opts or {}
+
+    -- before building
+    if config.autosave then
+        vim.cmd("silent write")
+    end
+
+    -- handle internal commands
+    local filetype = vim.bo.filetype
+    local cmd = config.commands[filetype]
+    local is_internal = config.enable_internals and vim.tbl_contains({ "lua", "vim" }, filetype)
+    if is_internal and not cmd then
+        vim.cmd("source %")
         return
     end
 
-    -- if config.behavior.autosave then
-    --     vim.cmd("silent write")
-    -- end
+    -- parse cmd
+    if not cmd then
+        info('Building "' .. filetype .. '" is not configured')
+        return
+    end
+    cmd = substitute(cmd)
 
-    -- cmd = substitute(cmd)
-    -- if type == "terminal" then
-    term(cmd)
-    -- end
+    -- preconfigure builder buffer
+    local position = opts.position or config.position
+    -- TODO: update size on vim resize (probably with autocommand)
+    local size = opts.size or config.size
 
-    -- vim.cmd("echohl ErrorMsg | echo 'Error: Invalid type' | echohl None")
-end
+    -- build/run the buffer
+    vim.cmd(position .. " " .. size .. "new | term " .. cmd)
 
-function M.Build(cmd)
-    -- local file = io.open(vim.fn.expand("%:p:h") .. "/.jaq.json", "r")
-
-    -- -- Check if the filetype is in config.cmds.internal
-    -- if vim.tbl_contains(vim.tbl_keys(config.cmds.internal), vim.bo.filetype) then
-    --     -- Exit if the type was passed and isn't "internal"
-    --     if type and type ~= "internal" then
-    --         vim.cmd("echohl ErrorMsg | echo 'Error: Invalid type for internal command' | echohl None")
-    --         return
-    --     end
-    --     type = "internal"
-    -- else
-    --     type = type or config.behavior.default
-    -- end
-
-    -- if type == "internal" then
-    --     internal()
-    --     return
-    -- end
-
-    run(cmd)
+    -- configure created buffer
+    local buf = vim.api.nvim_get_current_buf()
+    vim.bo[buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = buf, silent = true })
+    if not config.line_no then
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+    end
 end
 
 return M
